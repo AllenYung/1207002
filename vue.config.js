022cf0cb7d48f347229a940914f3884624dd23ab
AllenYung <a0968431969@gmail.com>
2023-12-07 15:38:48 +0800

    0338

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  // 其他設定...
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
+
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/1207mywedside/' : '/',
   }
